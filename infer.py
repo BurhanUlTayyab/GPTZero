b5e6a2dc3472782e5c6690a69ecd53bce51f36be
nicholas <nicholaschuawork@gmail.com>
2023-01-25 23:13:44 +0800

    Added a infer script

---
+++ infer.py
@@ -0,0 +1,17 @@
+"""
+This code a slight modification of perplexity by hugging face
+https://huggingface.co/docs/transformers/perplexity
+
+Both this code and the orignal code are published under the MIT license.
+
+by Burhan Ul tayyab and Nicholas Chua
+"""
+
+from model import GPT2PPL
+
+# initialize the model
+model = GPT2PPL()
+
+sentence = "your text here"
+
+model(sentence)
