c2a61364cf15164066ac748e43f8487c99a65339
nicholas <nicholaschuawork@gmail.com>
2023-01-27 17:10:13 +0800

    Added placeholder database

---
+++ database.py
@@ -0,0 +1,13 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+class DB:
+    def __init__(self):
+        """
+        Use a db of your choice
+        """
+        pass
+
+    def set(self, ip, email):
+        pass
