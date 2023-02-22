e6d5f107af2cd77b43ebcab0fde93df815d22efb
umair afzal <i190517@nu.edu.pk>
2023-02-22 15:09:36 +0500

    created a test case

---
+++ test_inference.py
@@ -0,0 +1,14 @@
+from model import GPT2PPL
+
+# initialize the model
+model = GPT2PPL()
+
+sentence = '''Takes in a sentence split by full stop and print the perplexity of the total sentence
+            split the lines based on full stop and find the perplexity of each sentence and print
+            average perplexity Burstiness is the max perplexity of each sentence'''
+
+result, out = model(sentence)
+
+assert out == "The Text is written by Human."
+
+#test
